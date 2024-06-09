ff46ed3c62296137a7271484c29a12af26c4893f
Nasaonsteroids <allan1.sikder@hotmail.com>
2024-06-10 00:10:46 +0200

    update index.js
    Minor fixes to the clock

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 function updateTime() {
-    var date = new Date();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-  
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
-  }
-  
-  setInterval(updateTime, 1000);
-  
+  var date = new Date();
+  var hours = date.getHours().toString().padStart(2, '0');
+  var minutes = date.getMinutes().toString().padStart(2, '0');
+  var seconds = date.getSeconds().toString().padStart(2, '0');
+
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}
+
+setInterval(updateTime, 1000);
